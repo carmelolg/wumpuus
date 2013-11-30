e2cb9aae44fefa13502926f5b46410c0819b181a
carmelolg <Linus@Linus-PC.homenet.telecomitalia.it>
2013-11-30 14:45:57 +0100

    second commit

--- Wumpuus/src/core/ProcessManagement.java
+++ Wumpuus/src/core/ProcessManagement.java
@@ -1,9 +1,36 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class ProcessManagement {
 
-	public static void main(String[] args) {
+	File file;
+	public ProcessManagement(File file) {
+		this.file = file;
+	}
+	private String answer_set() {
+		String answer_set = "";
 
+		try {
+			Process p = Runtime.getRuntime().exec("dlv.mingw.exe " + file.getName() + " -filter=CellaSafe");
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String tmp = "";
+			while ((tmp = input.readLine()) != null) {
+				answer_set += tmp;
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+		return answer_set;
+	}
+
+	public static void main(String[] args) {
+		File file = new File("wumpus.dlv");
+		ProcessManagement pm = new ProcessManagement(file);
+		System.out.println(pm.answer_set());
 	}
 
 }
